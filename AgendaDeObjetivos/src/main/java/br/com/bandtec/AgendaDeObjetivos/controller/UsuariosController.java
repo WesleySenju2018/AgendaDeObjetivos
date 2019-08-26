7364a1bd4935de5e7bd8ae2701cdcbc1e80ff50c
Aluno Bandtec <aluno@bandtec.com>
2019-08-26 20:58:55 -0300

    Aula de java web

--- AgendaDeObjetivos/src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/UsuariosController.java
+++ AgendaDeObjetivos/src/main/java/br/com/bandtec/AgendaDeObjetivos/controller/UsuariosController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -37,11 +39,20 @@ public class UsuariosController {
 	}
 
 	private List<Usuario> obterTodosUsuarios() {
-		return Arrays.asList(new Usuario("Wesley", 19), new Usuario("Henry", 1), new Usuario("Aline", 20));
+
+		ArrayList<Usuario> usuarios = new ArrayList<Usuario>();
+
+		usuarios.add(new Usuario("Wesley", 19));
+		usuarios.add(new Usuario("Brabo", 17));
+		usuarios.add(new Usuario("joon", 21));
+		/*
+		 * return Arrays.asList(new Usuario("Wesley", 19), new Usuario("Henry", 1), new
+		 * Usuario("Aline", 20));
+		 */
+		return usuarios;
+
 	}
 
-	
-	
 	@GetMapping("/usuarios/idade/{idadeDoUsuario}")
 	public ResponseEntity<List<Usuario>> obterPorIdade(@PathVariable("idadeDoUsuario") Integer idade) {
 		List<Usuario> usuariosPorIdade = new ArrayList<Usuario>();
@@ -59,5 +70,11 @@ public class UsuariosController {
 		}
 
 		return ResponseEntity.ok(usuariosPorIdade);
+	}
+
+	@PostMapping("/usuarios")
+	public ResponseEntity<Usuario> cadastrarUsuario(@RequestBody Usuario usuario) {
+		this.usuarios.add(usuario);
+		return ResponseEntity.ok(usuario);
 	}
 }
